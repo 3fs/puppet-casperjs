376fac41fcc8579ddf4fe36a40ffb70459c027bf
Martin Zibert <martin.zibert@3fs.si>
2013-11-11 19:29:34 +0100

    add $source_url in case you need to fetch a package from different url

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,13 @@
 class casperjs (
   $package_version = '1.0.3', # set package version to download
+  $source_url = "https://codeload.github.com/n1k0/casperjs/legacy.zip/$package_version"
   $source_dir = '/opt',
   $install_dir = '/usr/local/bin',
   $package_update = false,
 
 ) {
   exec { 'get casperjs':
-    command => "/usr/bin/curl --silent --show-error https://codeload.github.com/n1k0/casperjs/legacy.zip/$package_version --output $source_dir/casperjs.zip && mkdir $source_dir/casperjs && unzip $source_dir/casperjs.zip -d $source_dir && mv $source_dir/n1k0-casperjs-*/* $source_dir/casperjs/ && rm -rf $source_dir/n1k0-casperjs-*",
+    command => "/usr/bin/curl --silent --show-error $source_url --output $source_dir/casperjs.zip && mkdir $source_dir/casperjs && unzip $source_dir/casperjs.zip -d $source_dir && mv $source_dir/n1k0-casperjs-*/* $source_dir/casperjs/ && rm -rf $source_dir/n1k0-casperjs-*",
     creates => "$source_dir/casperjs/",
     require => Package['curl', 'unzip'],
   }
